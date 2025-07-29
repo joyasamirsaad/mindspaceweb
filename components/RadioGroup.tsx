0a782c3f8906e9f4923579912f6d13871512fbe6
joyasamirsaad <saad.joya.7@gmail.com>
2025-07-29 13:13:12 +0300

    Add files via upload

---
+++ components/RadioGroup.tsx
@@ -0,0 +1,28 @@
+interface RadioOption {
+  value: string;
+  label: string;
+}
+
+interface RadioGroupProps {
+  name: string;
+  legend: string;
+  options: RadioOption[];
+  selected?: string; 
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function RadioGroup({ name, legend, options, selected, onChange, }: RadioGroupProps) {
+  return (
+    <fieldset className="contactsections">
+      <legend>{legend}</legend>
+      <div>
+        {options.map((option, index) => (
+          <label key={index} className="radio-label">
+            <input type="radio" name={name} value={option.value} checked={selected === option.value} onChange={onChange} required />
+            {option.label}
+          </label>
+        ))}
+      </div>
+    </fieldset>
+  );
+}
