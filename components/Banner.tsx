479f325a6992db2c4fad4302d3c8b0877745e1ed
joyasamirsaad <saad.joya.7@gmail.com>
2025-07-29 14:46:46 +0300

    Add files via upload

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,3 +1,7 @@
+import { gsap, random } from "gsap";
+import { SplitText } from "gsap/SplitText";
+import { useEffect } from "react";
+
 type BannerProps = {
   bannerText: React.ReactNode;
   bgColor?: string;
@@ -5,9 +9,33 @@ type BannerProps = {
 };
 
 export default function Banner({ bannerText, bgColor, textColor }: BannerProps) {
+  useEffect(() => {
+    gsap.registerPlugin(SplitText);
+
+    // Wait for DOM to mount
+    const element = document.querySelector(".text");
+    if (!element) return;
+
+    const split = SplitText.create(element, { type: "words" });
+
+    gsap.from(split.words, {
+      y: 100,
+      autoAlpha: 0,
+      stagger: {
+        amount: 0.5,
+        from: "random", 
+      },
+      ease: "power2.out"
+    });
+
+    return () => {
+      split.revert(); // Clean up
+    };
+  }, []);
+  
   return (
     <section className="banner" style={{ backgroundColor: bgColor, color: textColor }}>
-      <div className="container mx-auto px-4 md:px-20 text-5xl">
+      <div className="container mx-auto px-4 md:px-20 text-5xl text">
         <p>{bannerText}</p>
       </div>
     </section>
