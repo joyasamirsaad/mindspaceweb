9649ed59441790bb53073113d5b458300649802a
joyasamirsaad <saad.joya.7@gmail.com>
2025-08-06 10:15:06 +0300

    Add files via upload

--- components/News.tsx
+++ components/News.tsx
@@ -69,12 +69,16 @@ function NewsList({ news }: NewsListProps) {
             simulateTouch={true}
             onSwiper={setSwiper}
             spaceBetween={20}
-            slidesPerView={2}
+            slidesPerView={1}
             /*breakpoints={{
               640: { slidesPerView: 1, spaceBetween: 20 },
               768: { slidesPerView: 3, spaceBetween: 24 },
               1024: { slidesPerView: 4, spaceBetween: 30 },
             }}*/
+           breakpoints={{
+                640: { slidesPerView: 1, spaceBetween: 20 },    
+                768: { slidesPerView: 2, spaceBetween: 24 },
+           }}
             onSlideChange={(swiper) => {
                 setAtStart(swiper.isBeginning);
                 setAtEnd(swiper.isEnd);
